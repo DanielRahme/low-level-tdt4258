e95fd7e1e4fedad52cb697a0f7ea298c04b6719f
vetlemt <55287185+vetlemt@users.noreply.github.com>
2019-09-24 14:40:59 +0200

    cleaned the code up. changed the code to follow the "official document".

--- ex1_support/ex1.s
+++ ex1_support/ex1.s
@@ -115,7 +115,7 @@ _reset:
 	ldr r5, =#0x802
 	str r1, [r0, #GPIO_EXTIPSELL]	// select interrupt pins
 	str r3, [r0, #GPIO_EXTIFALL]	// trigger at falling edge
-	str r2, [r0, #GPIO_EXTIRISE]	// trigger at falling edge
+	str r2, [r0, #GPIO_EXTIRISE]	// trigger at rising edge
 	str r2, [r0, #GPIO_IEN]			// enable interrupts
 	str r5, [r4]					// enable interrupt handling
 	
@@ -137,6 +137,10 @@ _reset:
 	
 	ldr r4, =state
 	mov r5, #1
+	str r5, [r4]
+	
+	ldr r4, =inverter
+	mov r5, #0xff00
 	str r5, [r4]
 	    
 /*------start of main code-------*/
@@ -161,71 +165,110 @@ state_select:
 	ldr r6, =state			// state address
 	ldr r5, [r4]			// load button press 
 
-	cmp r5, #0x01				// if button 1 
-	bne not_button1				// is not pressed skip
+	cmp r5, #0x01					// if button 1 is pressed
+	beq select_full_blink				 
+
+	cmp r5, #0x02					// if button 2 is pressed
+	beq select_alternating_blink 
+
+	cmp r5, #0x04					// if button 3 is pressed
+	beq select_invert				
+	
+	cmp r5, #0x08					// if button 4 is pressed
+	beq select_sleep			 
+
+	cmp r5, #0x10					// if button 5 is pressed 
+	beq select_left			 
+
+	cmp r5, #0x20					// if button 6 is pressed 
+	beq select_increase				 
+
+	cmp r5, #0x40					// if button 7 is pressed 
+	beq select_right				 
+
+	cmp r5, #0x80					// if button 8  is pressed
+	beq select_decrease				
+
+state_select_end:
+	bl reset_buttons
+	pop {lr}
+	bx lr    					// return
+
+//////////////////////////////////////////
+// selection options
+//////////////////////////////////////////
+
+select_left:
 	mov r7, #1					// else store left shift state (1)
 	str r7, [r6]				// to the state
-not_button1:
-	cmp r5, #0x02				// if button 2 
-	bne not_button2				// is not pressed skip
+	b state_select_end
+	
+select_led_positional_blink: 
 	ldr r8, =blinker			// else retrive blinker address
 	ldr r9, =leds				// and the leds address
 	ldr r10, [r9]				// retrive the current led position
 	str r10, [r8]				// store current led position as blinker
 	mov r7, #3					// store the blinker state (3)
-	str r7, [r6]				// to the state
-not_button2:
-
-	cmp r5, #0x04				// if button 3 
-	bne not_button3				// is not pressed skip
+	str r7, [r6]				// to the state	
+	b state_select_end
+	
+select_alternating_blink:		
 	ldr r8, =blinker			// else retrive blinker address
 	mov r10, #0x5500			// set alternating pattern
 	str r10, [r8]				// store alternating pattern as blinker
 	mov r7, #3					// store the blinker state (3)
-	str r7, [r6]				// to the state
-not_button3:
+	str r7, [r6]				// to the state	
+	b state_select_end
 
-	cmp r5, #0x08				// if button 4 
-	bne not_button4				// is not pressed skip
+select_full_blink:				
 	ldr r8, =blinker			// else retrive blinker address
 	mov r10, #0xff00			// set all leds to the same value
 	str r10, [r8]				// store same as blinker
 	mov r7, #3					// store the blinker state (3)
-	str r7, [r6]				// to the state
-not_button4:
-
-	cmp r5, #0x10				// if button 5 
-	bne not_button5				// is not pressed skip
-								// else do nothing
-not_button5:
-
-	cmp r5, #0x20				// if button 6 
-	bne not_button6				// is not pressed skip
-	bl increase_speed			// else increase the speed 
-not_button6:
-
-	cmp r5, #0x40				// if button 7 
-	bne not_button7				// is not pressed skip
+	str r7, [r6]				// to the state	
+	b state_select_end
+	
+select_increase:	
+	bl increase_speed			// increase the speed 
+	b state_select_end
+	
+select_right:			// is not pressed skip
 	mov r7, #2					// else store the right shift state
-	str r7, [r6]				// to the state
-not_button7:
-
-	cmp r5, #0x80				// if button 8 
-	bne not_button8				// is not pressed skip
+	str r7, [r6]				// to the state	
+	b state_select_end
+	
+select_decrease:				// is not pressed skip
 	bl decrease_speed			// else decrease the speed
-not_button8:
+	b state_select_end
+	
+select_invert:
+	ldr r7, =inverter
+	ldr r8, [r7]
+	mov r9, #0xff00
+	eor r8, r8, r9
+	str r8, [r7]
+	b state_select_end
+	
+select_sleep:
+	//// sleep code ///
+	b state_select_end	
+
+reset_buttons:
+	ldr r4, =buttons		// button address
 	mov r9, 0x0000				
 	str r9, [r4]				//reset buttons
-	pop {lr}
-	bx lr    					// return
-
-
+	bx lr
+	
+//////////////////////////////////////////	
+// states
+//////////////////////////////////////////
 		
 shift_led_right:
 	ldr r0, =GPIO_PA_BASE
 	ldr r5, =leds				// led address
 	ldr r2, [r5]				// led values
-	mov r3, #0xff00				// bits to toggle
+	ldr r7, =inverter
+	ldr r3, [r7]				// bits to toggle
 	eor r4, r2, r3				// toggle led bits
 	str r4, [r0, #GPIO_DOUT]	// write output leds
 	cmp r2, 0x8000				// checks if led is at the last stop
@@ -244,7 +287,8 @@ shift_led_left:
 	ldr r0, =GPIO_PA_BASE
 	ldr r5, =leds			// led address
 	ldr r2, [r5]			// led values
-	mov r3, #0xff00			// bits to toggle
+	ldr r7, =inverter
+	ldr r3, [r7]			// bits to toggle
 	eor r4, r2, r3			// toggle led bits
 	str r4, [r0, #GPIO_DOUT]// write output leds
 	cmp r2, 0x0100			// checks if led is at the last stop
@@ -257,7 +301,7 @@ left_end:
 
 reset_left:
 	mov r2, 0x8000			// reset led position
-	b right_end				// return to left shift loop			
+	b left_end				// return to left shift loop			
 			
 
 blink:
@@ -270,6 +314,10 @@ blink:
 	str r2, [r5]
 	bl delay
 	b main
+
+/////////////////////////////////////////////
+// other functions
+/////////////////////////////////////////////
 
 delay: 
 	ldr r3, =speed
@@ -342,4 +390,6 @@ speed:
 	.word 0
 state:
 	.word 0
+inverter:
+	.word 0	
 	

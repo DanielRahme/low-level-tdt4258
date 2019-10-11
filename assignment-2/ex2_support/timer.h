cbba4b6dbac248b6104f475e0118981876cf731a
Daniel Rahme <daniel.rahme.dr@gmail.com>
2019-10-11 14:23:53 +0200

    Cleanup and added interrupt status read function

---
+++ assignment-2/ex2_support/timer.h
@@ -0,0 +1,14 @@
+/*
+ * timer.h
+ *
+ * Created: 11.10.2019
+ * Author: Daniel
+ */ 
+
+#ifndef _TIMER_H_                              // HEADER GUARD
+#define _TIMER_H_
+
+void setupTimer(uint16_t period);
+int timerInterrupted();
+
+#endif

2cb50cde8bc061a02743c7da7d49802fdaaea6eb
vetlemt <55287185+vetlemt@users.noreply.github.com>
2019-11-21 18:17:36 +0100

    added collision.h

--- assignment-3/local_src/game-1.0/collision.c
+++ assignment-3/local_src/game-1.0/collision.c
@@ -1,46 +1,5 @@
 #include <stdbool.h>
-#include display.h
-#include game_objects.h
-/// collision detection
-
-#define DISP_HEIGHT 240
-#define DISP_WIDTH 320
-
-#define GOAL_LEFT 1
-#define GOAL_RIGHT 2
-#define PADDLE_WIDTH 32
-
-#define LEFT 0
-#define RIGHT (DISP_WIDTH - PADDLE_WIDTH - 1)
-#define TOP 0
-#define BOTTOM DISP_HEIGHT
-
-typedef struct{
-    int x;
-    int y;
-} velocity_t;
-
-typedef struct {
-    int x;
-    int y;
-    int width;
-    int height;
-} rect_t;
-
-typedef struct{
-    rect_t pos;
-    velocity_t velocity;
-    int color;
-}object_t;
-
-
-typedef struct
-{
-    int x_left;
-    int x_right;
-    int y_top;
-    int y_bottom;
-}abs_pos;
+#include "collision.h"
 
 
 abs_pos get_abs_pos(object_t *game_object){
@@ -61,8 +20,6 @@ bool detect_collision(object_t* object_1,object_t* object_2){
     return(  y_col & x_col );
 }
 
-#define LEFT 0
-#define RIGHT (DISP_WIDTH - PADDLE_WIDTH - 1)
 
 bool goal_collision(object_t *ball,int side) {
     object_t goal;
---
+++ assignment-3/local_src/game-1.0/collision.h
@@ -0,0 +1,29 @@
+
+#include "game_objects.h"
+#include "display.h"
+
+#define DISP_HEIGHT 240
+#define DISP_WIDTH 320
+
+#define GOAL_LEFT 1
+#define GOAL_RIGHT 2
+#define PADDLE_WIDTH 32
+
+#define LEFT 0
+#define RIGHT (DISP_WIDTH - PADDLE_WIDTH - 1)
+#define TOP 0
+#define BOTTOM DISP_HEIGHT
+
+typedef struct
+{
+    int x_left;
+    int x_right;
+    int y_top;
+    int y_bottom;
+}abs_pos;
+
+abs_pos get_abs_pos(object_t *game_object);
+bool detect_collision(object_t* object_1,object_t* object_2);
+bool goal_collision(object_t *ball,int side);
+bool frame_collision(object_t *ball,int side);
+
